c1c7753f9ff257a9b268733570150df7abaf2b54
aravind-7788 <59762113+aravind-7788@users.noreply.github.com>
2021-06-07 17:52:16 +0530

    Update App.java
    updated path for chromedirver

--- MYNEWTEST/src/main/java/MYNEWTEST/MYNEWTEST/App.java
+++ MYNEWTEST/src/main/java/MYNEWTEST/MYNEWTEST/App.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class App {
 	public static void main(String[] args) throws InterruptedException {
 		// System.out.println( "Hello World!" );
-		System.setProperty("webdriver.chrome.driver",
+		System.setProperty("webdriver.chrome.driver","\\usr\\bin\\chromedriver\\chromedriver.exe");
 				"C:\\Users\\user\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://34.67.212.0:8004/index.php");
